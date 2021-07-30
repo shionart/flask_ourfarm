2df4440fb6e632cb246f7130960e8b891266018f
shionart <malamin239@gmail.com>
2021-07-30 19:41:09 +0700

    notif tggl readunread

--- config/db.py
+++ config/db.py
@@ -2,6 +2,6 @@ from MySQLdb import cursors, connect
 import mysql.connector
 
 def connect_db():
-    conn = connect(host = "localhost", user="root", passwd="", db="db_sister", cursorclass=cursors.DictCursor)
+    conn = connect(host = "localhost", user="root", passwd="dragonica025", db="db_sister", cursorclass=cursors.DictCursor)
     # curr = conn.cursor()
     return conn
--- static/js/custom/notification.js
+++ static/js/custom/notification.js
@@ -1,17 +1,18 @@
 
 var isi=[];
 
-function generateCard(array){
+function generateCard2(array){
     var all="";
     for(a in array){
-        var id_arduino = array[a]['id_arduino'];
-        var soil_moist = array[a]['soil_moist'];
-        var time = array[a]['time'];
-        // getLastUpdated(id_arduino);
-    //    if (nama==null) {
-    //     nama="nama perangkat";
-    //    }
-       all +=   '<a class="dropdown-item d-flex align-items-center" href="#">'+
+      var id_arduino = array[a]['id_arduino'];
+      var soil_moist = array[a]['soil_moist'];
+      var time = array[a]['time'];
+      var notif = array[a]['notif'];
+      var nama = array[a]['nama'];
+      // if (notif==1) {
+        
+      // } else {
+        all +='<a class="dropdown-item d-flex align-items-center" href="'+notif_dashboard(id_arduino)+'">'+
        '<div class="mr-3">'+
          '<div class="icon-circle bg-warning">'+
            '<i class="fas fa-exclamation-triangle text-white"></i>'+
@@ -19,9 +20,11 @@ function generateCard(array){
        '</div>'+
        '<div>'+
          '<div class="small text-gray-500" id="">'+time+'</div>'+
-         '!Arduino '+id_arduino+' : Kelembapan tanah berada di '+soil_moist+'% !'+
+         'Perangkat <b>'+nama+'</b> : Kelembapan tanah berada di '+soil_moist+'%'+
        '</div>'+
-     '</a>';
+     '</a>';  
+      // }
+      
     }
     return all;
 }
@@ -34,7 +37,7 @@ function getNotified(){
             console.log('tidak ada notif');
         }
         else{
-            $("#listnotif").html(generateCard(isi));
+            $("#listnotif").html(generateCard2(isi));
         }
     });
 }
